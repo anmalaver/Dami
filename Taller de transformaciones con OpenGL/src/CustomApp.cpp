41d218951253a572306c029279f57d579672d68c
anmalaver <anmalaver@gmail.com>
2014-03-27 23:16:20 -0500

    Taller de transformaciones con OpenGL resuelto

---
+++ Taller de transformaciones con OpenGL/src/CustomApp.cpp
@@ -0,0 +1,66 @@
+#include "CustomApp.h"
+#include "InitialState.h"
+#include "TransformationsState.h"
+
+void CustomApp::setupInternal(){
+	ofSetFrameRate(60);
+
+	goToState(TRANSFORMATIONS_STATE);
+}
+
+void CustomApp::updateInternal(){
+}
+
+void CustomApp::drawInternal(){
+}
+
+void CustomApp::exitInternal(){
+}
+
+int CustomApp::getStateCount(){
+	return NUM_STATES;
+}
+
+State* CustomApp::createState(int id){
+	switch (id)	{
+		case INITIAL_STATE:
+			return new InitialState(this);
+
+		case TRANSFORMATIONS_STATE:
+			return new TransformationsState(this);
+	}
+	
+	return NULL;
+}
+
+string CustomApp::getStateName(int id){
+	return "";
+}
+
+bool CustomApp::isTransitionValid(State* from, State* to){
+	return true;
+}
+
+void CustomApp::keyPressed(int key){
+	StateMachineApp::keyPressed(key);
+}
+
+void CustomApp::keyReleased(int key){
+	StateMachineApp::keyReleased(key);
+}
+
+void CustomApp::mouseMoved(int x, int y ){
+	StateMachineApp::mouseMoved(x, y);
+}
+
+void CustomApp::mouseDragged(int x, int y, int button){
+	StateMachineApp::mouseDragged(x, y, button);
+}
+
+void CustomApp::mousePressed(int x, int y, int button){
+	StateMachineApp::mousePressed(x, y, button);
+}
+
+void CustomApp::mouseReleased(int x, int y, int button){
+	StateMachineApp::mouseReleased(x, y, button);
+}
